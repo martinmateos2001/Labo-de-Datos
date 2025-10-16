754ee2980b8bda6bbd3fcd9b99b0398dadd6d525
martinmateos2001 <165967918+martinmateos2001@users.noreply.github.com>
2025-10-16 18:36:53 -0300

    Add data processing for educational establishments

---
+++ tp.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import duckdb as dd
+import numpy as np
+
+# Establecimientos: Data frame de Establecimientos Educativos del padròn del 2022
+
+
+columnas_ee = 'A:C,L,N,U:AA'
+"""
+A - Jurisdiccion
+B - Cueanexo
+C - Nombre
+L - Departamento
+N - Común
+U - Jardin maternal
+V - Jardin de infantes
+W - Primario
+X - Secundario
+Y - Secundario - INET
+Z - SNU
+AA - SNU - INET
+"""
+
+
+Establecimientos = pd.read_excel("/home/Estudiante/Escritorio/tp labo datos//2022_padron_oficial_establecimientos_educativos.xlsx", skiprows=6, usecols= columnas_ee)
+
+
+
+consultaSoloComunes = """
+                      SELECT *
+                      FROM Establecimientos
+                      WHERE "Común" = '1'
+
+                      """
+
+Establecimientos = dd.sql(consultaSoloComunes).df()
+
+elimino = """
+            SELECT 'Jurisdicción' as Provincia, Departamento, 'Nivel inicial - Jardín maternal' as maternal,
+            'Nivel inicial - Jardín de infantes' as 'Jardin',Primario, Secundario,
+            'Secundario - INET' as 'SecundarioINET', 'SNU', 'SNU - INET' as 'SNUinet'
+            FROM df_establecimiento
+          """
+
+Establecimientos = dd.sql(elimino).df()
+print(Establecimientos)
