05d020948ac6151c16fd6b33fd35b150bc3d249f
martinmateos2001 <165967918+martinmateos2001@users.noreply.github.com>
2025-10-16 19:23:12 -0300

    Update file paths and SQL queries in tp.py

--- tp.py
+++ tp.py
@@ -22,24 +22,24 @@ AA - SNU - INET
 """
 
 
-Establecimientos = pd.read_excel("/home/Estudiante/Escritorio/tp labo datos//2022_padron_oficial_establecimientos_educativos.xlsx", skiprows=6, usecols= columnas_ee)
+Establecimientos = pd.read_excel("/home/Estudiante/Descargas//2022_padron_oficial_establecimientos_educativos.xlsx", skiprows=6, usecols= columnas_ee)
 
 
 
 consultaSoloComunes = """
                       SELECT *
                       FROM Establecimientos
-                      WHERE "Común" = '1'
+                      WHERE "Común" = '1';
 
                       """
 
 Establecimientos = dd.sql(consultaSoloComunes).df()
 
 elimino = """
-            SELECT 'Jurisdicción' as Provincia, Departamento, 'Nivel inicial - Jardín maternal' as maternal,
-            'Nivel inicial - Jardín de infantes' as 'Jardin',Primario, Secundario,
-            'Secundario - INET' as 'SecundarioINET', 'SNU', 'SNU - INET' as 'SNUinet'
-            FROM df_establecimiento
+            SELECT Jurisdicción as Provincia, Departamento, "Nivel inicial - Jardín maternal" as maternal,
+            "Nivel inicial - Jardín de infantes" as jardin, Primario, Secundario,
+            "Secundario - INET" as SecuInet, "SNU" as Snu, "SNU - INET" as SnuInet
+            FROM Establecimientos;
           """
 
 Establecimientos = dd.sql(elimino).df()
